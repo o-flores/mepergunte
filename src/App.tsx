2fcdec3bb30634ea916346c036c746aa22ba0bf6
Orlando Flores <orlando_flores@id.uff.br>
2021-06-22 12:55:36 -0300

    feat: providing authContext to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/rooms/new" component={ NewRoom } />
+        <AuthContextProvider>
+          <Route exact path="/" component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
       </Switch>
     </BrowserRouter>
   );
