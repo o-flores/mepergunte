4f18a35a54788f9d05603e8b2e08ac57d60b8e82
Orlando Flores <orlando_flores@id.uff.br>
2021-06-22 23:12:42 -0300

    feat: route to enter a existent room

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -10,7 +11,8 @@ function App() {
       <Switch>
         <AuthContextProvider>
           <Route exact path="/" component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
+          <Route exact path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
         </AuthContextProvider>
       </Switch>
     </BrowserRouter>
