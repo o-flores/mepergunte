b8d3d1923b43a3bbbec2273137143cd94a9e4ee8
Orlando Flores <orlando_flores@id.uff.br>
2021-06-23 12:53:01 -0300

    feat: create new component and style

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,37 @@
+import { useParams } from "react-router-dom";
+import { Button } from "../components/Button";
+import { RoomCode } from '../components/RoomCode';
+import '../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
+
+  const params = useParams<RoomParams>();
+
   return (
-    <h1>Sala</h1>
+    <div id="room-page">
+      <header>
+        <p>LOGO</p>
+        <div>
+          <RoomCode code={params.id} />
+        </div>
+      </header>
+      <main className="content">
+        <div className="room-title">
+          <h1>Nome da sala</h1>
+          <span>3 perguntas</span>
+        </div>
+        <form>
+          <textarea placeholder="Pergunte" />
+          <div className="form-footer">
+            <span>Para enviar sua pergunta, <button className="btn-login" > faça seu login.</button></span>
+            <Button type="submit">Enviar sua pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
