c187fefb2f48c9e858ba1fe4f549ce8c12b73915
Orlando Flores <orlando_flores@id.uff.br>
2021-06-22 23:11:41 -0300

    feat: creating a room with firebase

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,35 @@
+import { FormEvent, useState } from 'react';
 import { Button } from '../components/Button';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import illustrationImg from '../images/illustration.svg';
-
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
+  const { user } = useAuth();
+  const history = useHistory();
+
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+
+
+  }
+
   return (
     <div id="auth-page">
       <aside>
@@ -14,8 +40,10 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <h2>Crie uma nova sala</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input
+              onChange={({ target }) => setNewRoom(target.value)}
+              value={newRoom}
               type="text"
               placeholder="Código da sala"
             />
