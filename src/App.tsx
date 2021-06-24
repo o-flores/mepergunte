f6bd2f8c6030c090727f2d33aaeed7335fb66bbf
Orlando Flores <orlando_flores@id.uff.br>
2021-06-24 00:42:49 -0300

    feat: route to new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/mepergunte" component={Home} />
           <Route exact path="/room/new" component={NewRoom} />
           <Route exact path="/rooms/:id" component={Room} />
+          <Route exact path="/admin/rooms/:id" component={AdminRoom} />
         </AuthContextProvider>
       </Switch>
     </BrowserRouter>
